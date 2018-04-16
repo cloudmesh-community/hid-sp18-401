2e28a0a9ed11490f1641bada317b8155f5e711f1
GouthamSrivatsav <gauthamsrivatsav@gmail.com>
2018-04-15 22:11:51 -0400

    swagger-docker

---
+++ swagger-docker/default_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.cv import CV  # noqa: E501
+from swagger_server import util
+from Cross_Validation_Score import get_cross_validation_score
+
+def cv_get():  # noqa: E501
+    """cv_get
+
+    Returns cross validation average mean squared accuracy of linear regression model built on the &#39;Advertising&#39; dataset # noqa: E501
+
+
+    :rtype: CV
+    """
+    return CV(get_cross_validation_score())
